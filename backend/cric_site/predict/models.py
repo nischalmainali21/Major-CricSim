548a2efa3844b20642259c44a599b4104cf5ce1a
nischalmainali21 <nischalmainali21@gmail.com>
2024-04-10 11:24:23 +0545

    update lstm model in backend

--- backend/cric_site/predict/models.py
+++ backend/cric_site/predict/models.py
@@ -2,9 +2,9 @@ from django.db import models
 import torch.nn as nn
 
 class MyLSTMWithSoftmax(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size,num_layers):
         super(MyLSTMWithSoftmax, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size,num_layers=num_layers, batch_first=True)
         self.fc1 = nn.Linear(hidden_size, 32)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(32, output_size)
