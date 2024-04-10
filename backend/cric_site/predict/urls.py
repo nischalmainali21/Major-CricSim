069f3edafa39c76604f5c4a5c4d3fa43c34f0a82
Prakash-coder <chaulagainprakash78@gmail.com>
2024-04-10 15:53:39 +0545

    path to api

--- backend/cric_site/predict/urls.py
+++ backend/cric_site/predict/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import PredictionAPIView, PlotDataAPIView
-
+from .views2 import MatchSimulationAPIView
 urlpatterns = [
     path('full_data/', PredictionAPIView.as_view(), name='full_data'),
     path('plot_data/', PlotDataAPIView.as_view(), name='plot_data'),
+    path('team-api/',MatchSimulationAPIView.as_view(), name = 'team-api/')
 ]
